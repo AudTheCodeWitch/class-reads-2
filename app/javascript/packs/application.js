3daf3ce8c5ed19543a99f442bd5d2cd49d0e83fd
Audrea Cook <audreacwork@gmail.com>
2020-01-28 14:12:43 -0700

    Confiigure toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Import toastr in app/javascripts/packs/application.js:
+global.toastr = require("toastr");
+
+import "../stylesheets/application"
