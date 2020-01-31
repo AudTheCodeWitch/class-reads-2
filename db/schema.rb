29383fc46ee847011786e9c6d4b5d62bfd3e8ab4
Audrea Cook <audreacwork@gmail.com>
2020-01-31 15:56:17 -0700

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_212539) do
+ActiveRecord::Schema.define(version: 2020_01_31_221134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.bigint "teacher_id", null: false
+    t.string "title"
+    t.string "author"
+    t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teacher_id"], name: "index_books_on_teacher_id"
+  end
+
+  create_table "reading_responses", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "student_id", null: false
+    t.integer "page"
+    t.string "feeling"
+    t.string "thoughts"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_reading_responses_on_book_id"
+    t.index ["student_id"], name: "index_reading_responses_on_student_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "student_id", null: false
+    t.string "text"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["student_id"], name: "index_reviews_on_student_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.bigint "teacher_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teacher_id"], name: "index_students_on_teacher_id"
+    t.index ["user_id"], name: "index_students_on_user_id"
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_teachers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +79,12 @@ ActiveRecord::Schema.define(version: 2020_01_31_212539) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "teachers"
+  add_foreign_key "reading_responses", "books"
+  add_foreign_key "reading_responses", "students"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "students"
+  add_foreign_key "students", "teachers"
+  add_foreign_key "students", "users"
+  add_foreign_key "teachers", "users"
 end
