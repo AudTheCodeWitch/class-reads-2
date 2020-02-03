419631d1bc6dd8f109e0b965d6fca081fbd44e71
Audrea Cook <audreacwork@gmail.com>
2020-02-03 14:17:30 -0700

    Generate users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :books
   resources :students
   resources :teachers
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
