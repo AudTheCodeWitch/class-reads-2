e92902c5511e0990b246d30ef68b6a7b2438eb47
Audrea Cook <audreacwork@gmail.com>
2020-02-10 14:08:52 -0700

    Add has_one associations based on is_teacher boolean

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Table Associations
+  has_one :teacher, -> { where is_teacher: true }
+  has_one :student, -> { where is_teacher: false }
 end
