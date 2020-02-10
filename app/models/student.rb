dbcee7281377c06c2e842770eae4dd8bd57ea56b
Audrea Cook <audreacwork@gmail.com>
2020-02-10 14:09:59 -0700

    Add table associations for books, reviews, and responses

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :teacher
-  belongs_to :user
+  belongs_to :user, -> { where is_teacher: false }
+  has_many :reading_responses
+  has_many :reviews
+  has_many :books, through: :reviews
 end
