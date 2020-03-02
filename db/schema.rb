9079014951b253a16aedb180a2ed22fa97322ef9
Audrea Cook <audreacwork@gmail.com>
2020-03-02 15:24:49 -0700

    Add name to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_225143) do
+ActiveRecord::Schema.define(version: 2020_03_02_220840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_225143) do
     t.boolean "administrator_role", default: false
     t.boolean "teacher_role", default: false
     t.boolean "student_role", default: true
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
